f54fdc80a9624b10d3d16ef2f5be5c832f67d81f
Ian Cahn <iancahn@live.com>
2022-09-21 22:47:16 +0200

    Updated TwoArrays challenge
    Found a better solution, instead of trying to go this route where I separate strings and numbers, its better to concat the 2 arrays, and then filter then with a double true parameter using OR.
    
    I failed this one, but learned a lot.

--- twoarrays.js
+++ twoarrays.js
@@ -5,20 +5,38 @@
 //Note: You can return the array with its elements in any order.
 
 function diffArray(arr1, arr2) {
-    let arrOne = arr1.sort();
-    let arrTwo = arr2.sort();
-    let arrThree = [];
-    let arrFour = [];
-    let arrFinal = [];
-    arrThree = arrOne.filter(item => !arrTwo.includes(item));
-    console.log("arrThree is now: " + arrThree);
-    arrFour = arrTwo.filter(item => !arrOne.includes(item));
-    console.log("arrFour is now: " + arrFour);
-    arrFinal = [arrThree.concat(arrFour)];
-    console.log("This is the answer " + arrFinal);
-    return arrFinal;
+    return arr1
+        .concat(arr2)
+        .filter(item => !arr1.includes(item) || !arr2.includes(item));
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
-//diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+//     let arrOne = arr1.sort();
+//     let arrTwo = arr2.sort();
+//     let arrThree = [];
+//     let arrFour = [];
+//     let arrFinal = [];
+//     arrThree = arrOne.filter(item => !arrTwo.includes(item));
+//     let textThree = arrThree.join(", ");
+//     if (typeof textThree === 'string' || textThree instanceof String) {
+//         console.log("textThree is a string");
+//     }
+//     console.log("arrThree is now: " + arrThree);
+//     arrFour = arrTwo.filter(item => !arrOne.includes(item));
+//     let textFour = arrFour.join(", ");
+//     if (typeof textFour === 'string' || textFour instanceof String) {
+//         console.log("textFour is a string");
+//     }
+//     console.log("arrFour is now: " + arrFour);
+//     arrFinal = [arrThree.join()];
+//     for (let k = 0; k < arrFour.length; k++) {
+//         arrFinal.push(arrFour[k])
+//     }
+//     console.log("This is the answer " + arrFinal);
+//     return arrFinal;
+// }
+
+//diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+// this answer should be 4
+
+diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
 //diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
